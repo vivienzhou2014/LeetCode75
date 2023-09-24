eb9924d8fc7ad3f5b8126b406ce9397166679075
vivienzhou2014 <136516629+vivienzhou2014@users.noreply.github.com>
2023-09-24 00:43:01 -0600

    Add files via upload

--- DetermineifTwoStringsAreClose1657.java
+++ DetermineifTwoStringsAreClose1657.java
@@ -1,8 +1,8 @@
-import java.util.HashMap;
+import java.util.*;
 
 
 public class DetermineifTwoStringsAreClose1657 {
-    public boolean closeStrings(String word1, String word2) {
+    public boolean closeStrings1(String word1, String word2) {
         //This problem is actually to check 3 thing
         //1. the length of 2 strings are the same
         if(word1.length() != word2.length()){
@@ -18,12 +18,50 @@ public class DetermineifTwoStringsAreClose1657 {
         for (int i = 0; i < word2.length(); i++) {
             hm2.put(word2.charAt(i), hm2.getOrDefault(word2.charAt(i), 0) + 1);
         }
-        //3. the number of each charactors are the same(does not have to be same char)
-        //    so we can put occurance of each char in word 1 in hashmap1, same for word2, and check if their values equals
         if(!hm1.keySet().equals(hm2.keySet())){
             return false;
         }
-        
+        //3. the number of each charactors are the same(does not have to be same char)
+        //    so we can put occurance of each char in word 1 in hashmap1, same for word2, and check if their values equals
+        List<Integer> occ1 = new ArrayList<>();
+        List<Integer> occ2 = new ArrayList<>();
+        for(Object key: hm1.keySet()){
+            occ1.add(hm1.get(key));
+        }
+        for(Object key: hm2.keySet()){
+            occ2.add(hm2.get(key));
+        }
+        Collections.sort(occ1);
+        Collections.sort(occ2);
+        if(!occ1.equals(occ2)){
+            return false;
+        }
 
+        return true;
+
+    }
+    public boolean closeStrings(String word1, String word2) {
+        //the previous method used hashmap and arraylist, but we just need 26 lower case numbers
+        //so an int[] is good enough for that
+        //we just need to put int[0] for the occurance of a... int[25] for the occurence of z
+        //then check if any place is empty for one but not empty for another
+        //then we sort the two arrays to compare
+        int[] occ1 = new int[26];
+        int[] occ2 = new int[26];
+        for (char c : word1.toCharArray()){
+            occ1[c - 'a']++;
+        }
+        for(char c : word2.toCharArray()){
+            occ2[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if((occ1[i] + occ2[i]) > 0 && (occ1[i] == 0 || occ2[i] == 0)){
+                return false;
+            }
+        }
+        Arrays.sort(occ1);
+        Arrays.sort(occ2);
+
+        return Arrays.equals(occ1, occ2);
     }
 }
