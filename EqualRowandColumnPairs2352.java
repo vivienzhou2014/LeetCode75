b3f8663b47f2adfa842eb5a6cccee3171488398d
vivienzhou2014 <136516629+vivienzhou2014@users.noreply.github.com>
2023-09-25 23:50:47 -0600

    Add files via upload
    This time I thought of a solution but it has bugs

--- EqualRowandColumnPairs2352.java
+++ EqualRowandColumnPairs2352.java
@@ -1,5 +1,37 @@
-public class EqualRowandColumnPairs2352 {
-    public int equalPairs(int[][] grid) {
+import java.util.Arrays;
 
+public class EqualRowandColumnPairs2352 {
+    public static int equalPairs(int[][] grid) {
+        //my idea is to use each row(grid[0] grid[1]..) compare to each column(grid[0][0], [1][0]..)
+        //so I will flip to matrix to make columns to rows and then compare each rows of two matrix
+        //to flip make grid[i][j] to grid[j][i]
+        int n = grid.length;
+        //int[][] flippedGrid = new int[n][n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //flippedGrid[j][i] = grid[i][j];
+                if(grid[i][j] == grid[j][i]){
+                    count++;
+                }
+            }
+        }
+
+        //now compare rows
+        //int count = 0;
+/*        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j] == flippedGrid[i][j]){
+                    count++;
+                }
+            }
+        }*/
+        //if found one ==, count++
+        return count/n;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{3,2,1},{1,7,6},{2,7,7}};
+        System.out.println(equalPairs(grid));
     }
 }
